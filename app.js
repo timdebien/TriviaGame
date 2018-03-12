0d17aff53531d75ca9e6a4c5c96f9737f7a98d08
Timothy Debien <timothydebien@Timothys-MacBook-Pro.local>
2018-03-11 23:16:10 -0600

    Trivia

---
+++ app.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+  //number of seconds on the timer
+  var numberSecs = 30;
+  var intervalId;
+  //questions and answers
+  var correct = 0;
+  var wrong = 0;
+  var none = 0;
+  var questCount = 0;
+  //all questions answer and correct
+  var allQuestions = [
+    {
+      question: "What is the largest bone in the body?",
+      answers: ["Femur", "Ulina", "Radius", "Fibula"],
+      correctAns: "Femur"
+    },
+    
+    {
+      question: "How many chambers does the heart have?",
+      answers: ["1", "3", "4", "5"],
+      correctAns: "4"
+    },
+
+    {
+      question: "The stomach empties into the ?",
+      answers: ["Jejunum", "Ileum", "Duodenum", "Transverse colon"],
+      correctAns: "Duodenum"
+    },
+
+    {
+      question: "What vein empties directly into the brain?",
+      answers: ["Brachiocephalic", "Internal jugular", "External jugular", "Subclavian"],
+      correctAns: "Internal jugular"
+    },
+
+    {
+      question: "The anatomical name for the thumb is ?",
+      answers: ["Hallux", "Pollex", "Phalanges ", "Thumpkin"],
+      correctAns: "Pollex"
+    },
+
+    {
+      question:  "All of the following are bones of the skull except:",
+      answers: ["Frontal", "Parietal", "Ischium", "Sphenoid",],
+      correctAns: "Ischium"
+    },
+    
+    {
+      question: "The largest internal organ of the body is the",
+      answers: ["Liver", "Lungs", "Spleen", "Gallbladder"],
+      correctAns: "Liver"
+    },
+
+    {
+      question: "All these hormones are excreted from the anterior pituitary gland except:",
+      answers: ["Luteinizing hormone", "Follicle-stimulating hormone", "Gonadotropin-releasing hormone", "Antidiuretic hormone"],
+      correctAns: "Antidiuretic hormone"
+    },
+
+    {
+      question: "There are 4 parts of the spine.  Which one of the following is not one of them?",
+      answers: ["Cephalic", "Sacral", "Thoracic", "Lumbar"],
+      correctAns: "Cephalic"
+    },
+
+    {
+      question: "The deep muscles of the abdomen are called",
+      answers: ["Internal oblique", "Rectus abdominis", "Transverse abdominis", "Linea alba"],
+      correctAns: "Transverse abdominis"
+    }
+
+
+  ]
+
+  
+    function run() {
+    clearInterval(intervalId)
+     intervalId = setInterval(decrement, 1000)
+  }
+   function decrement() {
+    //decrease number by one
+    numberSecs--;
+    //show number in #timer id
+     $("#timer").html("<h2>" + numberSecs + "</h2>")
+    //number hits zero...
+    if (numberSecs === 0) {
+      stop();
+       
+      };
+  };
+  
+   function stop() {
+      clearInterval(intervalId);
+    }
+    run();
+
+  function correctAnswer() {
+    correct++;
+    questCount++;
+    displayQuestion();
+
+  }
+  function wrongAnswer() {
+    wrong++;
+    questCount++;
+
+    displayQuestion();
+  }
+
+  function displayQuestion() {
+    $(".question").text(allQuestions[questCount].question)
+    $(".answer1").text(allQuestions[questCount].answers[0])
+    $(".answer2").text(allQuestions[questCount].answers[1])
+    $(".answer3").text(allQuestions[questCount].answers[2])
+    $(".answer4").text(allQuestions[questCount].answers[3])
+
+    $(".button").on("click", function () {
+      if ($(this).text() === allQuestions[questCount].correctAns) {
+        correctAnswer();
+      }
+      else {
+        wrongAnswer();
+      }
+
+
+    })
+  }
+  $(".startbutton").text("click here to start")
+  $(".startbutton").on("click", function () {
+    displayQuestion()
+
+  });
+
+
+});//end document ready
